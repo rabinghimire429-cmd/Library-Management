2fd035ec8773a03ae63d0662bfefc2520d3b26de
aayushapandit68 <aayushapandit68@gmail.com>
2026-05-07 21:30:53 +0200

    Added comments to delete-book page

--- book/delete-book.php
+++ book/delete-book.php
@@ -1,13 +1,24 @@
 <?php
+
+// Start session
 session_start();
+
+// Check admin login
 if(!isset($_SESSION['admin_id'])) {
     header('Location: ../index.php');
     exit();
 }
 
+// Database connection
 $conn = new mysqli('localhost', 'root', '', 'libtech_db');
+
+// Get book id
 $id = $_GET['id'];
+
+// Delete book from database
 $conn->query("DELETE FROM book WHERE book_id = $id");
+
+// Redirect back to search page
 header('Location: search-books.php');
 exit();
 ?>
