7434293d75795081987c0887ad388d9a8d1c50c4
Rabin Ghimire <rabinghimire429@gmail.com>
2026-05-22 14:00:28 +0200

    complete database file

--- database.sql
+++ database.sql
@@ -1,3 +1,11 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 22, 2026 at 11:51 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
